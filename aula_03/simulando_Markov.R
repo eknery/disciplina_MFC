e84777dfe86dcca4d26f375623cdd0ee972c6a89
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2024-09-13 18:17:19 -0300

    atualização

---
+++ aula_03/simulando_Markov.R
@@ -0,0 +1,130 @@
+############################ APRESENTAÇÃO DO MODELO ############################
+
+## Parâmetros do modelo de Markov
+# Estados 
+estados <- c("0", "1", "2")
+# taxas de transição entre estados (em milhões de anos)
+matriz_q <- matrix(c(1, 1, 1,
+                     1, 1, 1,
+                     1, 1, 1), 
+                   nrow = 3, byrow = TRUE)
+rownames(matriz_q) <- estados
+colnames(matriz_q) <- estados
+# ver  matriz de transição
+matriz_q
+
+## Cenário temporal
+# tempo final (em milhões de anos)
+tf <- 10
+# tempo atual 
+ti <- 0
+# vetor para armazenar o tempo de todos os estados
+ts = ti
+
+# estado atual
+yi = "0"
+## vetor para armazenar todos os estados
+Y = yi
+
+while (ti < tf) {
+    # Taxa de saída do estado atual
+    taxa <- matriz_q[yi, yi]
+    
+    # Tempo até a próxima transição 
+    dt <- rexp(1, rate = taxa)
+    
+    # Rodar simulação até o tempo final
+    if (ti + dt > tf) break
+      
+      # Atualização de tempo
+      ti <- ti + dt
+      ts <- c(ts, ti)
+    
+      # Probabilidades de transição para os outros estados
+      probs_transicao <- matriz_q[yi, ]
+      probs_transicao[yi] <- 0  # Não podemos mudar para o estado atual
+      probs_transicao <- probs_transicao / taxa  # Normalizar para probabilidades
+    
+      # Escolher o próximo estado
+      yi <- sample(estados, 1, prob = probs_transicao)
+      # Armazenar estado novo
+      Y <- c(Y, yi)
+}
+
+plot(x = ts, 
+     y = Y, 
+     type = "s",
+     col = "blue",
+     xlab = "Tempo (milhões de anos)", 
+     ylab = "Estado de Y",
+     )
+
+############################# SIMULANDO EVOLUÇÃO LENTA #########################
+
+source("simula_markov.R")
+
+## Parâmetros do modelo de Markov
+# Estados 
+estados <- c("0", "1", "2")
+# taxas de transição entre estados (em milhões de anos)
+matriz_q2 <- matrix(c(0.1, 0.5, 0.5,
+                      0.5, 0.1, 0.5,
+                      0.5, 0.5, 0.1), 
+                   nrow = 3, byrow = TRUE)
+rownames(matriz_q2) <- estados
+colnames(matriz_q2) <- estados
+# ver  matriz de transição
+matriz_q2
+
+# gerando processo de Markov
+markov2 <- simula_markov(matriz = matriz_q2, 
+                         y0 = 0,
+                         tf = tf)
+
+plot(x = ts, 
+     y = Y, 
+     type = "s",
+     col = "blue",
+     xlab = "Tempo (milhões de anos)", 
+     ylab = "Estado de Y",
+)
+lines(x = markov2$ts,
+     y = markov2$Y,
+     type = "s",
+     col = "red",
+)
+
+##################### SIMULANDO TENDÊNCIAS E RESTRIÇÕES ########################
+
+source("simula_markov.R")
+
+## Parâmetros do modelo de Markov
+# Estados 
+estados <- c("0", "1", "2")
+# taxas de transição entre estados (em milhões de anos)
+matriz_q3 <- matrix(c(1, 1, 0,
+                      1, 5, 1,
+                      0, 1, 1), 
+                    nrow = 3, byrow = TRUE)
+rownames(matriz_q3) <- estados
+colnames(matriz_q3) <- estados
+# ver  matriz de transição
+matriz_q3
+
+# gerando processo de Markov
+markov3 <- simula_markov(matriz = matriz_q3, 
+                         y0 = 0,
+                         tf = tf)
+
+plot(x = ts, 
+     y = Y, 
+     type = "s",
+     col = "blue",
+     xlab = "Tempo (milhões de anos)", 
+     ylab = "Estado de Y",
+)
+lines(x = markov3$ts,
+      y = markov3$Y,
+      type = "s",
+      col = "darkgreen",
+)
--- aula_03/simulando_OU.R
+++ aula_03/simulando_OU.R
@@ -1,25 +1,145 @@
-# Parâmetros do modelo OU
-theta <- 10   # Tendência central
-alpha <- 0.15 # Taxa de reversão 
-sigma <- 0.2  # Taxa evolutiva
-ti <- 1         # tempo final (em milhões de anos)
-N <- 10000      # Número de passos no tempo 
-dt <- ti / N    # Tamanho do passo (gerações)
-ts <- seq(0, t_fim, by = dt)  # Linha do tempo
+############################ APRESENTAÇÃO DO MODELO ############################
 
-# Vetor para armazenar os valores do processo
-X <- numeric(length(t))
-X[1] <- 1 # Valor inicial do processo OU
+## Parâmetros do modelo OU
+# Tendência central
+theta <- 1  
+# Taxa de reversão
+alpha <- 0.25 
+# Taxa de variação
+sigma <- 3
 
-# Simulação do processo OU
+## Cenário temporal
+# tempo final (em milhões de anos)
+tf <- 1 
+# intervalo de tempo entre geração (em milhões de anos)
+dt <- 0.0001  
+# vetor com o tempo de todas as gerações
+ts <- seq(0, tf, by = dt)  
+
+## vetor para armazenar os valores da característica
+Y <- c() 
+# valor inicial da característica
+Y[1] <- 1 
+
+## Simulação do processo OU
 for (i in 2:length(ts)) {
-  dW <- rnorm(1, mean = 0, sd = sigma )  # Incremento browniano
-  X[i] <- X[i-1] + (alpha * (theta - X[i-1])) * dt + (sigma * dW)
+  dS <- dt * rnorm(1, mean = 0, sd = sigma)  # variação estocástica
+  dD <- dt * (alpha * (theta - Y[i-1]) ) # variação determinística
+  Y[i] <- Y[i-1] + dD + dS
 }
 
-# Plot do processo OU
-plot(ts, X, type = "l", 
+## Plot do processo OU
+plot(x = ts, 
+     y = Y, 
+     ylim = c(0.9,1.1),
+     type = "l", 
      col = "blue", 
-     ain = "Simulação do Processo Ornstein-Uhlenbeck",
-     xlab = "Tempo", 
-     ylab = "Característica X")
+     
+     xlab = "Tempo (milhões de anos)", 
+     ylab = "Valor de Y",
+     main = paste0("Processo OU ",
+                   " theta: ", theta, 
+                   " alpha: ", alpha,
+                   " sigma: ", sigma
+                  )
+     )
+lines(x = ts, 
+      y = rep(theta, length(ts)),
+      col = "blue",
+      type = "l", 
+      lty = 2)
+
+#################### SIMULANDO DIFERENTES ÓTIMOS ADAPTATIVOS ###################
+
+## Parâmetros do modelo OU
+# Tendência central
+theta2 <- 1.05
+# Taxa de reversão
+alpha2 <- 0.25 
+# Taxa de variação
+sigma2 <- 3
+
+## Vetor para armazenar os valores da característica
+Y2 <- numeric(length(ts))
+# valor inicial da característica
+Y2[1] <- 1 
+
+## Simulação do processo OU
+for (i in 2:length(ts)) {
+  dS <- dt * rnorm(1, mean = 0, sd = sigma2)  # variação estocástica
+  dD <- dt * (alpha2 * (theta2 - Y[i-1]) ) # variação determinística
+  Y2[i] <- Y2[i-1] + dD + dS
+}
+
+## Plot do processo OU
+plot(x = ts, 
+     y = Y, 
+     ylim = c(0.9,1.1),
+     type = "l", 
+     col = "blue", 
+     xlab = "Tempo (milhões de anos)", 
+     ylab = "Característica Y",
+     main = paste0(
+              "theta1: ", theta, "  ",
+              "theta2: ", theta2
+              )
+     )
+lines(x = ts, 
+      y = Y2, 
+      type = "l", 
+      col = "red")
+lines(x = ts, 
+      y = rep(theta, length(ts)),
+      col = "blue",
+      type = "l", 
+      lty = 2)
+lines(x = ts, 
+      y = rep(theta1, length(ts)),
+      col = "red",
+      type = "l", 
+      lty = 2)
+
+################### SIMULANDO DIFERENTES PRESSÕES SELETIVAS ####################
+
+## Parâmetros do modelo OU
+# Tendência central
+theta3 <- 1
+# Taxa de reversão
+alpha3 <- 0.0025 
+# Taxa de variação
+sigma3 <- 3
+
+## Vetor para armazenar os valores da característica
+Y3 <- numeric(length(ts))
+# valor inicial da característica
+Y3[1] <- 1 
+
+## Simulação do processo OU
+for (i in 2:length(ts)) {
+  dS <- dt * rnorm(1, mean = 0, sd = sigma3)  # variação estocástica
+  dD <- dt * (alpha3 * (theta3 - Y[i-1]) ) # variação determinística
+  Y3[i] <- Y3[i-1] + dD + dS
+}
+
+## Plot do processo OU
+plot(x = ts, 
+     y = Y, 
+     ylim = c(0.9,1.1),
+     type = "l", 
+     col = "blue", 
+     xlab = "Tempo (milhões de anos)", 
+     ylab = "Característica Y",
+     main = paste0(
+       "alpha1: ", alpha, "  ",
+       "alpha3: ", alpha3
+        )
+     )
+lines(x = ts, 
+      y = Y3, 
+      type = "l", 
+      col = "purple")
+lines(x = ts, 
+      y = rep(theta, length(ts)),
+      col = "blue",
+      type = "l", 
+      lty = 2)
