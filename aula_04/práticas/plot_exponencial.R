8cb276960ce789b865baee33556167414b008301
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2024-11-22 22:44:34 -0300

    update

--- aula_04/práticas/plot_exponencial.R
+++ aula_04/práticas/plot_exponencial.R
@@ -1,29 +1,25 @@
-### definindo função exponencial
-fx_exponencial = function(x, lambda) {lambda*exp(1)^(-lambda*x)}
 
 ### definindo parâmetos
 x_range <- seq(0, 10, by=0.1)
 
-q_vl = 0.32
+q = 0.4
 ### Criando o gráfico
 plot(x = x_range, 
-     y= dexp(x = x_range, rate = q_vl, log = FALSE) , 
-     type="l", col="darkgray",lwd= 6,
+     y= dexp(x = x_range, rate = q, log = FALSE) , 
+     type="l", col="darkred",lwd= 6,
      main = paste0("vermelho para laranja, q = ", q_vl),
-     xlab="tempo até a transição", ylab="p(x)"
-)
-
-q_vr = 0.35
-### Criando o gráfico
-plot(x = x_range, 
-     y= dexp(x = x_range, rate = q_vr, log = FALSE) , 
-     type="l", col="darkgray",lwd= 6,
-     main = paste0("vermelho para roxo, q = ", q_vr),
      xlab="tempo até a transição", ylab="p(x)"
 )
 
 
 nloglik<- function(x,q) sum(-dexp(x=x,rate=q,log=T))
 
-xs = c(2.25)
-optimize(f= nloglik,x= xs,interval = c(0,10))
+xs = c(1,1)
+
+
+dexp(x=xs,rate=q,log=T)
+
+optimize(f= nloglik, x= xs,interval = c(0,1))
+
+
+
