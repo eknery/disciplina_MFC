e329106e2d3deaf74cccf73632b27252d3a6ad16
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2024-12-06 11:04:52 -0300

    update

--- aula_04/auxiliar/plot_exponencial.R
+++ aula_04/auxiliar/plot_exponencial.R
@@ -2,13 +2,13 @@
 ### definindo parâmetos
 x_range <- seq(0, 10, by=0.1)
 
-q = 0.15
+q = 0.19
 ### Criando o gráfico
 plot(x = x_range, 
      y= dexp(x = x_range, rate = q, log = FALSE) , 
-     type="l", col="purple",lwd= 6,
-     ylim= c(0, 0.55),
-     main = paste0("saída de (R), q = ", q),
+     type="l", col="darkred",lwd= 6,
+     ylim= c(0, 0.20),
+     main = paste0("saída de (V), q = ", q),
      xlab="tempo até a transição", ylab="p(x)",
      cex.axis=1,cex.lab=1.2
 )
