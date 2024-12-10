9ce24481a5e443ccf96917def903d8baeec409c5
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2024-12-10 13:41:27 -0300

    update

--- aula_02/auxiliar/mle_OU.R
+++ aula_02/auxiliar/mle_OU.R
@@ -5,13 +5,13 @@ likelihood_ou_two_species <- function(x1, x2, t, s, sigma2, alpha, theta) {
   mean_vector <- c(theta, theta)
   
   # Calcula os elementos da matriz de covariância
-  v <- (sigma2 / (2 * alpha)) * (1 - exp(-2 * alpha * t)) # Variância
-  c <- (sigma2 / (2 * alpha)) * (1 - exp(-2 * alpha * s) * exp(-2 * alpha * (t-s) ))     # Covariância
+  v <-  (1 - exp(-2 * alpha * t)) # Variância
+  c <-  (1 - exp(-2 * alpha * s) * exp(-2 * alpha * (t-s) )) # Covariância
   
   # Matriz de covariância
-  cov_matrix <- matrix(c(v, c, 
-                         c, v), 
-                       nrow = 2, ncol = 2)
+  cov_matrix <- (sigma2 / (2 * alpha)) * matrix(c(v, c, 
+                                                  c, v), 
+                                                 nrow = 2, ncol = 2)
   
   # Vetor de traços observados
   traits <- c(x1, x2)
