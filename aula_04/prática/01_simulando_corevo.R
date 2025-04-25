944fad40056e5f8ffe1319919f8e08ae388e4104
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2025-04-25 08:42:28 -0300

    updates

--- aula_04/prática/01_simulando_corevo.R
+++ aula_04/prática/01_simulando_corevo.R
@@ -4,13 +4,6 @@
 
 ######################## SIMULANDO EVOLUÇÃO CORRELACIONADA  ####################
 
-# 1) Produza uma simulação com os valores iniciais fornecidos no sript.
-# 
-# 2) Produza uma simulação com uma correlação evolutiva 10X menor que o valor inicial. 
-#
-# 3) Produza uma simulação com uma correlação evolutiva 10X menor que o valor inicial,
-# e com uma taxa evolutiva 10X menor para a característica Z.
-
 ## Cenário temporal 
 # tempo total (em milhões de anos)
 tf <- 1 
@@ -23,10 +16,10 @@ ts <- seq(0, tf, by = dt)
 # Taxa de variação de Y
 sigma_sqY <- 0.05 # inicial: 0.05
 # Taxa de variação de Z
-sigma_sqZ <- 0.05 # inicial: 0.05
+sigma_sqZ <- 0.005 # inicial: 0.05
 
 ## Correlação evolutiva entre Y e Z
-covYZ <- 0.5 # incial: 0.5
+corevo <- 0.07 # incial: 0.7
 
 ## vetores para armazenar os valores das características
 Y <- c() 
@@ -39,8 +32,8 @@ Z <- 1
 for (i in 2:length(ts)) {
   dY <-  rnorm(1, mean = 0, sd = sqrt(sigma_sqY) )  # variação estocástica de Y
   dZ <-  rnorm(1, mean = 0, sd = sqrt(sigma_sqZ) )  # variação estocástica de Z
-  Y[i] <- Y[i-1] + dY + (sqrt(covYZ) * dZ) 
-  Z[i] <- Z[i-1] + dZ + (sqrt(covYZ) * dY)
+  Y[i] <- Y[i-1] + dY + (corevo * dZ) 
+  Z[i] <- Z[i-1] + dZ + (corevo * dY)
 }
 
 ## Plot da simulação
@@ -54,7 +47,7 @@ plot(x = ts,
      main = paste0("BM",
                    "; sigma_sq Y: ", sigma_sqY,
                    "; sigma_sq Z: ", sigma_sqZ,
-                   "; cov YZ: ", covYZ
+                   "; Corr Evo: ", corevo
                    
      )
 )
@@ -63,3 +56,25 @@ lines(x = ts,
       type = "l", 
       col = "orange")
 
+## EM GRUPO:
+
+## Vamos verificar os valores finais das características
+Y[length(ts)]
+Z[length(ts)]
+
+# Vamos armazenar os valores finais de Y e Z das nossas simulações:
+Ys = c(-11.6, -17.4, -2.49, -22.6)
+Zs = c(-11.9, -8.0, 14.0, 2.6)
+
+# Vamos calcular a correlação padrão entre as características
+cor(Ys,Zs)
+
+# Agora vamos comparar a correlação padrão e a correlação evolutiva.
+# Os valores são similares? 
+c("Padrão" = cor(Ys,Zs), "Evolutiva" = corevo)
+
+## NOVOS EXPERIMENTOS:
+# 2) Produza uma simulação com uma correlação evolutiva 10X menor que o valor inicial.
+#
+# 3) Produza uma simulação com uma correlação evolutiva 10X menor que o valor inicial,
+# e com uma taxa evolutiva 10X menor para a característica Z. 
