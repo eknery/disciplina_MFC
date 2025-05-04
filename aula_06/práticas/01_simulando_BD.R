08b7a0a4ad57f2f20eb4fee59cd26edfd03b6f22
Eduardo Koerich Nery <91353128+eknery@users.noreply.github.com>
2025-05-04 12:34:27 -0300

    Update 01_simulando_BD.R

--- aula_06/práticas/01_simulando_BD.R
+++ aula_06/práticas/01_simulando_BD.R
@@ -3,27 +3,27 @@
 
 ## Parâmetros do modelo PB (Yule)
 # Taxa de nascimento
-lambda1 <- 0.3
+lambda1 = 0.3
 
 ## Cenário temporal
-t <- 0  # tempo atual
+t = 0  # tempo atual
 ts1 = t  # tempo de todos os eventos
-tf <- 20 # tempo máximo da simulação
+tf = 20 # tempo máximo da simulação
 
 ## Cenário de diversidade
-n <- 1   # número de espécies atual
-N1 <- n  # número de espécies ao longo da simulação
+n = 1   # número de espécies atual
+N1 = n  # número de espécies ao longo da simulação
 
 # Simulação do processo Pure Birth
 while (t < tf) {
   # tempos até o próximos evento de nascimento
-  t_birth <- rexp(1, n * lambda1)
+  t_birth = rexp(1, n * lambda1)
   # atualizando o tempo
-  t <- t + t_birth
-  ts1 <- c(ts1, t)
+  t = t + t_birth
+  ts1 = c(ts1, t)
   # atualizando a diversidade
-  n <- n + 1 # 'nasce' uma espécie
-  N1 <- c(N1, n)
+  n = n + 1 # 'nasce' uma espécie
+  N1 = c(N1, n)
 }
 
 ## Plot da simulação
@@ -41,43 +41,43 @@ plot(
 )
 
 # Veja o número final de espécies da simulação
-Nb= N1[length(N1)]
-Nb
+Nf1 = N1[length(N1)]
+Nf1
 
 ############################## MODELO BIRTH-DEATH #############################
 
 ## Parâmetros do modelo BD
 # Taxa de nascimento
-lambda2 <- 0.35
+lambda2 = 0.35
 # Taxa de morte
-mu2 <- 0.05 
+mu2 = 0.05 
 
 ## Cenário temporal
-t <- 0  # tempo atual
+t = 0  # tempo atual
 ts2 = t  # tempo de todos os eventos
-tf <- 20 # tempo máximo da simulação
+tf = 20 # tempo máximo da simulação
 
 ## Cenário de diversidade
-n <- 1   # número de espécies atual
-N2 <- n  # número de espécies ao longo da simulação
+n = 1   # número de espécies atual
+N2 = n  # número de espécies ao longo da simulação
 
 # Simulação do processo Birth-Death
 while (t < tf) {
   # tempos até os próximos eventos de nascimento e morte
-  t_birth <- rexp(1, n * lambda2)
-  t_death <- rexp(1, n * mu2)
+  t_birth = rexp(1, n * lambda2)
+  t_death = rexp(1, n * mu2)
   # qual evento ocorre antes?
-  ti <- min(t_birth, t_death)
+  ti = min(t_birth, t_death)
   # atualizando o tempo
-  t <- t + ti
-  ts2 <- c(ts2, t)
+  t = t + ti
+  ts2 = c(ts2, t)
   # atualizando a diversidade
   if (ti == t_birth) {
-    n <- n + 1 # 'nasce' uma espécie
+    n = n + 1 # 'nasce' uma espécie
   } else {
-    n <- n - 1 # 'morre' uma espécie
+    n = n - 1 # 'morre' uma espécie
   }
-  N2 <- c(N2, n)
+  N2 = c(N2, n)
 }
 
 ## Plot da simulação
@@ -96,8 +96,8 @@ plot(
 )
 
 # Veja o número final de espécies da simulação
-Nbd = N2[length(N2)]
-Nbd
+Nf2 = N2[length(N2)]
+Nf2
 
 ############################### COMPARANDO MODELOS #############################
 
@@ -120,5 +120,28 @@ lines(
   col = "red"
 )
 
+## EM GRUPO
 
+# Vamos armazenar os valores finais de diversidade dos dois tipos de modelo:
+Nb = c()
+Nbd = c()
+
+# Vamos armazenar os intervalos de tempo entre as especiações:
+tesp1 = ts1
+tesp2 = ts2[diff(N2) == 1]
+
+hist(
+  tesp1 ,
+  col = rgb(0,0,1, 0.5),
+  breaks = 10,
+  xlab = "Tempo entre especiações (milhões de anos)", 
+  ylab = "Frequência",
+  main = paste0("Pure Birth: Azul;  BD: Vermelho")
+)
+hist(
+  tesp2 ,
+  col = rgb(1,0,0, 0.5),
+  breaks = 10,
+  add = T
+)
 
